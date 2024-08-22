d06d97e61c12a2c510b88cb19ec32845dc5f34d5
BONAFO <91232074+BONAFO@users.noreply.github.com>
2024-08-22 13:07:44 -0300

    Add files via upload

--- Apí Starter/backend.bat
+++ Apí Starter/backend.bat
@@ -1,7 +1,10 @@
 @echo off
+@REM Disco donde esta la api (C/D/E)
 DiscoDondeSeUbicaLaApi:
+@REM Ruta completa de done esta la api C:\Users\{usuario}\Desktop\Api\env\Scripts
 cd RutaDelBack\env\Scripts
 call activate.bat && (
+@REM Ruta completa de done esta la api C:\Users\{usuario}\Desktop\Api
    cd RutaDelBack
    python manage.py runserver
 )
--- Apí Starter/frontend.bat
+++ Apí Starter/frontend.bat
@@ -1,6 +1,9 @@
 @echo off
+@REM Disco donde esta la api (C/D/E)
 DiscoDondeSeUbicaLaApi:
+@REM Ruta completa de done esta la api C:\Users\{usuario}\Desktop\Api
 cd RutaDelFront
+@REM Comando de npm requerido para iniciar el servidor
 npm run serve
 
 
